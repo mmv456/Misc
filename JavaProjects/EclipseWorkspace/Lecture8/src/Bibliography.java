e968b2bd88e248139c5a1c07d4f8909ab0c3683b
Valluru <mahitha.valluru@gmail.com>
2022-04-06 22:13:54 -0400

    new class

--- JavaProjects/EclipseWorkspace/Lecture8/src/Bibliography.java
+++ JavaProjects/EclipseWorkspace/Lecture8/src/Bibliography.java
@@ -1,4 +1,37 @@
-
-public class Bibliography {
-
+interface Bibliography {
+	
 }
+
+class Empty implements Bibliography {
+	
+}
+
+class Article implements Bibliography {
+	String author;
+	String title;
+	String URL;
+	Bibliography documents;
+	
+	Article(String author, String title, String URL, Bibliography documents) {
+		this.author = author;
+		this.title = title;
+		this.URL = URL;
+		this.documents = documents;
+	}
+	
+}
+
+class Book implements Bibliography {
+	String author;
+	String title;
+	String publisher;
+	Bibliography documents;
+	
+	Book(String author, String title, String publisher, Bibliography documents) {
+		this.author = author;
+		this.title = title;
+		this.publisher = publisher;
+		this.documents = documents;
+	}
+}
+
